a7d5eb5c3de8167c560408fed0b480282873f1c7
Katelyn McDaniels <katemcd@uark.edu>
2020-04-02 17:19:52 -0500

    updated code to use csv module to parse the file and argparse so that user can use the parogram on any gff and fasta file, instead of hardcoding a single file in

--- parseGFF.py
+++ parseGFF.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python3
 
-infile = 'watermelon.gff'
+#import modules
+import csv
+import argparse
 
-with open(infile, 'r') as melon:
-    gene = []
-    for line in melon:
-        line = line.rstrip('\n')
-        fields = line.split('\t')
-        if (fields[2] != 'misc_feature' and fields[2] != 'repeat_region'):
-            parse = fields[8].split(' ')
-            gene.append(parse[1])
+#create and argument parse object
+parser = argparse.ArgumentParser(description = 'This script will take a .gff file, pull out the gene names from the list, and store them in a alphabatized list. Then it will take that list and use it to pull out the gene sequences from a fasta file and calculate the GC content.')
 
-    gene.sort()
+#add positional arguments for the gff and fasta files to be used
+parser.add_argument('gff', help= 'The name of the .gff file you wish to use', type=str)
+parser.add_argument('fasta', help= "The name of the fasta file you wish to use", type=str)
+
+#parse the arguments
+args = parser.parse_args()
+#define files
+gff_file = 'watermelon.gff'
+fasta_file = 'watermelon.fsa'
+#open and parse the gff file, and define the list
+with open(args.gff, 'r') as gff:
+    gene_names = []
+    reader = csv.reader(gff, delimiter= '\t')
+    for line in reader:
+        if not line:
+            continue
+        else:
+            #separate out the repeated or similar to genes
+            if (line[2] != 'misc_feature' and line[2] != 'repeat_region'):
+               #pull out gene name and add it to the list
+                parse = line[8].split(' ')
+                gene_names.append(parse[1])
+
+#print the gene games in alphabetical order
+for gene in sorted(gene_names):
     print(gene)
+
+gff.close
+
+#open and close the fasta file
+with open(args.fasta, 'r')as fasta:
+    
+    fasta.close
