14cdabe22b52153dfb179bdff29bb44a3d5c8bbf
Katelyn McDaniels <katemcd@uark.edu>
2020-04-07 15:02:11 -0500

    edited scrip to pull out specific genes and calculate the GC content

--- parseGFF.py
+++ parseGFF.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 #import modules
 import csv
 import argparse
+from Bio import SeqIO
 
 #create and argument parse object
 parser = argparse.ArgumentParser(description = 'This script will take a .gff file, pull out the gene names from the list, and store them in a alphabatized list. Then it will take that list and use it to pull out the gene sequences from a fasta file and calculate the GC content.')
@@ -14,27 +15,34 @@ parser.add_argument('fasta', help= "The name of the fasta file you wish to use",
 #parse the arguments
 args = parser.parse_args()
 
-#open fasta file
-fasta = open(args.fasta, 'r')
+#read and parse the fasta file
+genome = SeqIO.read(args.fasta, 'fasta')
 
 #open and parse the gff file, and define the list
 with open(args.gff, 'r') as gff:
-    genes = []
     reader = csv.reader(gff, delimiter= '\t')
     for line in reader:
         if not line:
             continue
         else:
-            #separate out the repeated or similar to genes
-            if (line[2] != 'misc_feature' and line[2] != 'repeat_region'):
-               #pull out gene name and add it to the list
+            #test to see if gene is CDS feature
+            if (line[2] != 'CDS'):
+               continue
+            else:
                 parse = line[8].split(' ')
-                genes.append(parse[1] + " " + line[3] + " " + line[4])
+                #pull out the start and stop position for the gene
+                start = int(line[3])
+                stop = (int(line[4]) + 1)
+                #extract sequence region for gene
+                seq = genome.seq[start:stop]
+                #calculate GC content
+                g_count = seq.count('G')
+                c_count = seq.count('C')
+                total = len(seq)
+                gc_total = g_count + c_count
+                content = float(gc_total) / total
+                #print gene name and GC content
+                print(parse[1] + " GC content = " + str('{0:2f}'.format(content)))
 
-#print the gene games in alphabetical order
-for gene in sorted(genes):
-    print(gene)
-
-#close both files
-gff.close()
-fasta.close()
+#close all files
+gff.close()                
