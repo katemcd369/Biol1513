f3647ab0024118d5d753a764deebbf4767727285
Katelyn McDaniels <katemcd@uark.edu>
2020-04-10 19:13:09 -0500

    Edited parseGFF.py so that it was modularized. Added chapter 7 exercises from Python for Biologists

--- parseGFF.py
+++ parseGFF.py
@@ -5,44 +5,59 @@ import csv
 import argparse
 from Bio import SeqIO
 
-#create and argument parse object
-parser = argparse.ArgumentParser(description = 'This script will take a .gff file, pull out the gene names from the list, and store them in a alphabatized list. Then it will take that list and use it to pull out the gene sequences from a fasta file and calculate the GC content.')
 
-#add positional arguments for the gff and fasta files to be used
-parser.add_argument('gff', help= 'The name of the .gff file you wish to use', type=str)
-parser.add_argument('fasta', help= "The name of the fasta file you wish to use", type=str)
+def get_args():
+    #create and argument parse object
+    parser = argparse.ArgumentParser(description = 'This script will take a .gff file, pull out the gene names from the list, and store them in a alphabatized list. Then it will take that list and use it to pull out the gene sequences from a fasta file and calculate the GC content.')
 
-#parse the arguments
-args = parser.parse_args()
+    #add positional arguments for the gff and fasta files to be used
+    parser.add_argument('gff', help= 'The name of the .gff file you wish to use', type=str)
+    parser.add_argument('fasta', help= "The name of the fasta file you wish to use", type=str)
 
-#read and parse the fasta file
-genome = SeqIO.read(args.fasta, 'fasta')
+    #parse the arguments
+    return parser.parse_args()
 
-#open and parse the gff file, and define the list
-with open(args.gff, 'r') as gff:
-    reader = csv.reader(gff, delimiter= '\t')
-    for line in reader:
-        if not line:
-            continue
-        else:
-            #test to see if gene is CDS feature
-            if (line[2] != 'CDS'):
-               continue
+def parse_fasta():
+    #read and parse the fasta file
+    return SeqIO.read(args.fasta, 'fasta')
+
+def parse_gff(genome):
+    #open and parse the gff file, and define the list
+    with open(args.gff, 'r') as gff_file:
+        reader = csv.reader(gff_file, delimiter= '\t')
+        for line in reader:
+            if not line:
+                continue
             else:
-                parse = line[8].split(' ')
-                #pull out the start and stop position for the gene
-                start = int(line[3])
-                stop = (int(line[4]) + 1)
-                #extract sequence region for gene
-                seq = genome.seq[start:stop]
-                #calculate GC content
-                g_count = seq.count('G')
-                c_count = seq.count('C')
-                total = len(seq)
-                gc_total = g_count + c_count
-                content = float(gc_total) / total
-                #print gene name and GC content
-                print(parse[1] + " GC content = " + str('{0:2f}'.format(content)))
+                #test to see if gene is CDS feature
+                if (line[2] == 'CDS'):
+                    parse = line[8].split(' ')
+                    #pull out the start and stop position for the gene
+                    start = (int(line[3]) - 1)
+                    stop = int(line[4])
+                    #extract sequence region for gene
+                    seq = genome[start:stop]
+                    feature_GC = gc(seq)
+                    #print gene name and GC content
+                    print(parse[1] + " GC content = " + str('{0:2f}'.format(feature_GC)))
+                    
+def gc(sequence):
+    #calculate GC content
+    g_count = sequence.count('G')
+    c_count = sequence.count('C')
+    total = len(sequence)
+    gc_total = g_count + c_count
 
-#close all files
-gff.close()                
+    return (float(gc_total) / total)
+
+def main():
+    genome_sequence = parse_fasta()
+    parse_gff(genome_sequence.seq)
+
+#get the command line arguments before calling main
+args = get_args()
+
+#exicute program by calling main
+if __name__ == "__main__":
+    main()
+              
